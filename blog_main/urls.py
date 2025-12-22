11d32d45f3c3e1bdeb80f633fd94fcaead3f7e8a
Praveentelukala <praveentelukala2003@gmail.com>
2025-12-22 23:49:12 +0530

    dashboard and categories crud functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -31,6 +31,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
+
+     # Dashboards
+    path('dashboard/', include('dashboards.urls')),
 ]
 
 if settings.DEBUG:
